37c12c759aeaafc8ace40ff19e4010ef3f02f2fc
Thomas GERMAIN <thomasgermain07@hotmail.fr>
2020-06-17 12:10:56 +0200

    20.06.17 12:10:56: ---

--- srcs/pipe_parsing.c
+++ srcs/pipe_parsing.c
@@ -6,7 +6,7 @@
 /*   By: thgermai <thgermai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 11:52:31 by thgermai          #+#    #+#             */
-/*   Updated: 2020/06/16 16:30:43 by thgermai         ###   ########.fr       */
+/*   Updated: 2020/06/17 12:08:14 by thgermai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,29 +37,57 @@ static int		get_n_pipes(char *args, int option)
 	return (n_pipes);
 }
 
-static void		exec_pipes(char **tab)
+static void		exec_process(char **func, int t_pro, int n_pro, int fd[t_pro - 1][2])
 {
-	int			fd[2]; // fd[0] read | fd[1] write
-	char		**call;
-
-	if (pipe(fd) == -1)
-		exit(1);
-	if (fork() == 0)
+	if (n_pro == 0)
 	{
-		call = ft_split(tab[0], ' ');
-		close(fd[0]);
-		dup2(fd[1], 1);
-		close(fd[1]);
-		execvp(call[0], call);
+		close(fd[0][0]);
+		dup2(fd[0][1], 1);
+		close (fd[0][1]);
+	}
+	else if (n_pro == t_pro - 1)
+	{
+		close(fd[n_pro - 1][1]);
+		dup2(fd[n_pro - 1][0], 0);
+		close(fd[n_pro - 1][0]);
 	}
 	else
 	{
-		call = ft_split(tab[1], ' ');
-		close(fd[1]);
-		dup2(fd[0], 0);
-		close(fd[0]);
-		execvp(call[0], call);
+		close(fd[n_pro - 1][1]);
+		close(fd[n_pro][0]);
+		dup2(fd[n_pro - 1][0], 0);
+		dup2(fd[n_pro][1], 1);
+		close(fd[n_pro - 1][0]);
+		close(fd[n_pro][1]);
 	}
+	int ret = execvp(func[0], func);
+	printf("exec failed with error %d\n", ret);
+}
+
+static void		exec_pipes(char **tab, int n_pipe)
+{
+	char		**call;
+	int			fd[n_pipe][2]; // fd[0] read | fd[1] write
+	int			i;
+	pid_t		pid;
+
+	i = -1;
+	while (++i < n_pipe)
+		if (pipe(fd[i]) == -1)
+			exit(1);
+	i = -1;
+	while (++i < n_pipe + 1)
+	{
+		call = ft_split(tab[i], ' ');
+		if ((pid = fork()) == 0)
+		{
+			exec_process(call, n_pipe + 1, i, fd);
+			printf("if printed there is a problem\n"); // del
+		}
+		else
+			waitpid(pid, NULL, 0);
+	}
+	exit(0);
 }
 
 void			check_pipes(char *args)
@@ -67,6 +95,7 @@ void			check_pipes(char *args)
 	char		*str[get_n_pipes(args, 0) + 2];
 	int			i;
 	int			last_i;
+	pid_t		pid;
 
 	i = 0;
 	last_i = 0;
@@ -78,8 +107,8 @@ void			check_pipes(char *args)
 	}
 	str[i] = ft_substr(args + last_i, 0, ft_strlen(args + last_i));
 	str[i + 1] = NULL;
-	if (fork() == 0)
-		exec_pipes(str);
+	if ((pid = fork()) == 0)
+		exec_pipes(str, get_n_pipes(args, 0));
 	else
-		wait(NULL);
+		waitpid(pid, NULL, 0);
 }
