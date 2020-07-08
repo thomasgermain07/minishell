b617aeb37fb512db7a820c159df776ce0d5afc13
Thomas Germain <thomasgermain07@hotmail.fr>
2020-07-08 20:07:36 +0200

    rest

--- srcs/prompt_utiles.c
+++
@@ -1,6 +0,0 @@
-#include "../includes/minishell.h"
-
-void			print(void)
-{
-	ft_printf("\033[1;32mMINISHELL \033[0m 👉 ");
-}
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("this will return 123\n");
-	return (123);
-}
